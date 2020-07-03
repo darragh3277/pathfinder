7c637d12127a9078545c8a780a998aede0d77d62
Darragh <darragh3277@gmail.com>
2020-07-03 13:52:58 +0100

    hoisted grid state to app root

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import PropTypes from "prop-types";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Sidebar from "./components/sidebar/Sidebar";
@@ -13,12 +13,16 @@ const useStyles = makeStyles(() => ({
   },
 }));
 
+const blockDimension = 10; //size of each block
+
 function Pathfinder() {
   const classes = useStyles();
   const [mobileOpen, setMobileOpen] = useState(false);
   const [selectedAlgorithm, setSelectedAlgorithm] = useState("");
   const [selectedGrid, setSelectedGrid] = useState("");
-  const [selectedSpeed, setSelectedSpeed] = useState("");
+  const [selectedSpeed, setSelectedSpeed] = useState("Fast");
+  const [grid, setGrid] = useState([]);
+  const gridRef = useRef();
 
   const handleAlgorithmChange = (e) => {
     setSelectedAlgorithm(e.target.value);
@@ -36,6 +40,36 @@ function Pathfinder() {
     setMobileOpen(!mobileOpen);
   };
 
+  useEffect(() => {
+    if (grid.length === 0) {
+      setGrid(buildGrid());
+    }
+  });
+
+  const buildGrid = () => {
+    let width = gridRef.current.clientWidth;
+    let height = gridRef.current.clientHeight;
+    let numRows = Math.floor(width / blockDimension);
+    let numCols = Math.floor(height / blockDimension);
+    const empty = 0;
+    const start = 1;
+    const end = 2;
+    const wall = 3;
+
+    let row = [];
+    for (let i = 0; i < numCols; i++) {
+      row.push(0);
+    }
+    let grid = [];
+    for (let i = 0; i < numRows; i++) {
+      grid.push(row);
+    }
+    return grid;
+    // grid[10][10] = start;
+    // grid[20][20] = end;
+    // console.log(grid);
+  };
+
   return (
     <div className={classes.root}>
       <CssBaseline />
@@ -50,7 +84,7 @@ function Pathfinder() {
         handleGridChange={handleGridChange}
         selectedGrid={selectedGrid}
       />
-      <Grid />
+      <Grid gridRef={gridRef} />
     </div>
   );
 }
--- src/components/grid/Grid.js
+++ src/components/grid/Grid.js
@@ -1,7 +1,6 @@
-import React, { useRef, useEffect } from "react";
+import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
-import Dijkstra from "../../algorithms/Dijkstra";
 
 const useStyles = makeStyles((theme) => ({
   // necessary for content to be below app bar
@@ -18,44 +17,14 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const Grid = () => {
+const Grid = (props) => {
   const classes = useStyles();
-  const gridRef = useRef();
-  const blockDimension = 10; //size of each block
-
-  useEffect(() => {
-    let width = gridRef.current.clientWidth;
-    let height = gridRef.current.clientHeight;
-    let rows = Math.floor(width / blockDimension);
-    let cols = Math.floor(height / blockDimension);
-    buildGrid(rows, cols);
-  });
-
-  const buildGrid = (numRows, numCols) => {
-    const empty = 0;
-    const start = 1;
-    const end = 2;
-    const wall = 3;
-
-    let row = [];
-    for (let i = 0; i < numCols; i++) {
-      row.push(0);
-    }
-    let grid = [];
-    for (let i = 0; i < numRows; i++) {
-      grid.push(row);
-    }
-    // grid[10][10] = start;
-    // grid[20][20] = end;
-    console.log(grid);
-  };
+  const { gridRef } = props;
 
   return (
     <main className={classes.content}>
       <div className={classes.toolbar} />
-      <Container ref={gridRef} className={classes.gridContainer}>
-        <div id="test">sddf</div>
-      </Container>
+      <Container ref={gridRef} className={classes.gridContainer}></Container>
     </main>
   );
 };
